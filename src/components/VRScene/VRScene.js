61b4f929370c017ba02eee76c150e9e06dacccb0
Rotem Harel <me@rotemharel.com>
2017-12-21 23:08:14 +0100

    adding a camera and a cursor

--- src/components/VRScene/VRScene.js
+++ src/components/VRScene/VRScene.js
@@ -6,10 +6,19 @@ import './vrScene.styl';
 
 const VRScene = () => (
   <Scene>
+    <Entity position={{ x: 0, y: -1, z: 1 }}>
+      <a-camera>
+        <a-entity
+          position="0 0 -1"
+          geometry="primitive: ring; radiusInner: 0.02; radiusOuter: 0.03"
+          material="color: white; shader: flat"
+        />
+      </a-camera>
+    </Entity>
     <Entity geometry={{ primitive: 'box' }} material={{ color: '#66296b' }} position={{ x: 0, y: 0, z: -5 }} />
     <Entity particle-system={{ preset: 'snow' }} />
     <Entity light={{ type: 'point' }} />
-    <Entity text={{ value: 'Hello, WebVR!' }} />
+    <Entity text={{ value: 'Hello, WebVR!' }} position={{ x: 0, y: 1, z: 0 }} />
   </Scene>
 );
 
